cf169f2df643fc047a23a9cdfdb3b0b7c18e8d34
ashwanitiwari <cooltiwari690@gmail.com>
2018-07-01 03:41:02 +0530

    #05 add taxes and new functions

--- application/controllers/api/invoice.php
+++ application/controllers/api/invoice.php
@@ -35,10 +35,19 @@ class Invoice extends CI_Controller
                             // print_r($product);
                                 $products = $this->where->select_where($product[0],["ID"=>$product[1]],"NAME");
                             }
+                            $ac_id = $this->where->select_where("accounts",["REFRENCE"=>$data['REF']],"ID");
+                            $discount = $this->where->select_where("accounts",["REFRENCE"=>$data['DISCOUNT_REF']],"AMOUNT");
+                            $tax = $this->where->select_where("accounts","REFRENCE LIKE '%invoice_".$ac_id[0]['ID']."_tax%'","AMOUNT,CR_ID");
+                            $tax_data = array();
+                            foreach((array)$tax as $tax):
+                                $tax_name = $this->where->select_where("accouting_ledgers","ID='". $tax['CR_ID']."' GROUP BY NAME","NAME,COUNT(NAME) AS TOTAL_TAX");
+                                $tax_data[] = array("NAME"=>$tax_name[0]['NAME'],"AMOUNT"=>$tax['AMOUNT']);
+                            endforeach;
                             $account_ref = $this->where->select_where("accounts",["REFRENCE"=>$data['REF']],"AMOUNT");
                             $response['data'][] = array("ID"=>$data['ID'],"PRODUCT_ID"=>$data['PRODUCT_ID'],"QUANTITY"=>$data['QUANTITY'],"PRODUCT"=>$products[0]['NAME'],"DATE"=>$data['DATE'],
                                                             "INVOICE_NO"=>$data['INVOICE_NO'],"OP_ID"=>$data['OP_ID'],"REF"=>$data['REF'],"AMOUNT"=>$account_ref[0]['AMOUNT'],
-                                                            "TYPE"=>$data['TYPE'],"SUBSCRIBER_ID"=>$data['SUBSCRIBER_ID'], "RATE" => $data['RATE'], "DISCOUNT" => $data['DISCOUNT'], "DISCOUNT_SCOPE" => $data['DISCOUNT_SCOPE']);
+                                                            "TYPE"=>$data['TYPE'],"SUBSCRIBER_ID"=>$data['SUBSCRIBER_ID'], "RATE" => $data['RATE'], "DISCOUNT" => $discount[0]['AMOUNT'], "DISCOUNT_SCOPE" => $data['DISCOUNT_SCOPE']
+                                                        , "TAX" => $tax_data, "TOTAL_TAX"=>$tax_name[0]['TOTAL_TAX']);
                             endforeach;
                                             echo json_encode($response);
                         }
@@ -119,6 +128,9 @@ class Invoice extends CI_Controller
                         $response = array("status"=>1,"response"=>array("ENTRY_ID"=>$id,"INVOICE_NO"=>$invoice_no[0]['LAST_INVOICE']+1),"errors"=>array("count"=>0,null));
                         echo json_encode($response);
                     }
+                    else{
+                        echo $this->api->errors(101);
+                    }
                 }
             else
                 {
@@ -141,6 +153,9 @@ class Invoice extends CI_Controller
                             echo $this->api->errors(104);
                         }
                     }
+                    else{
+                        echo $this->api->errors(101);
+                    }
                 }
             else
                 {
@@ -153,7 +168,7 @@ class Invoice extends CI_Controller
         {
             if($this->api->authentication())
                 {
-                    if(isset($_POST['OP_ID'], $_POST['SUBSCRIBER_ID'], $_POST['INVOICE_NO'], $_POST['PRODUCT_ID'], $_POST['QUANTITY'], $_POST['REF'], $_POST['TYPE'], $_POST['RATE'], $_POST['DISCOUNT'], $_POST['DISCOUNT_SCOPE']) && !in_array("",$_POST))
+                    if(isset($_POST['OP_ID'], $_POST['SUBSCRIBER_ID'], $_POST['INVOICE_NO'], $_POST['PRODUCT_ID'], $_POST['QUANTITY'], $_POST['REF'], $_POST['TYPE'], $_POST['RATE'], $_POST['DISCOUNT_REF'], $_POST['DISCOUNT_SCOPE']) && !in_array("",$_POST))
                     { 
                         // $invoice_no = $this->where->select_where("invoice_summary","OP_ID=".$_POST['OP_ID'],"MAX(INVOICE_NO) AS LAST_INVOICE");
                         // $data = array(
@@ -167,7 +182,7 @@ class Invoice extends CI_Controller
                         // echo json_encode($response);
                         if(sizeof($_POST<=11))
                         {
-                          if($this->where->check_where_num("accounts","REFRENCE='".$_POST['REF']."'")==1 && $this->where->check_where_num("invoice","REF='".$_POST['REF']."'")==0 && $this->where->check_where_num("invoice_summary","INVOICE_NO=".$_POST['INVOICE_NO']." AND OP_ID = ".$_POST['OP_ID']." AND SUBSCRIBER_ID = ".$_POST['SUBSCRIBER_ID']." AND STATUS = 0") && $this->where->check_where_num("products",["ID"=>$_POST['PRODUCT_ID']])==1)
+                          if($this->where->check_where_num("accounts","REFRENCE='".$_POST['REF']."'")==1 && $this->where->check_where_num("invoice","REF='".$_POST['REF']."'")==0 && $this->where->check_where_num("invoice_summary","INVOICE_NO=".$_POST['INVOICE_NO']." AND OP_ID = ".$_POST['OP_ID']." AND SUBSCRIBER_ID = ".$_POST['SUBSCRIBER_ID']." AND STATUS = 0") && $this->where->check_where_num("products",["ID"=>$_POST['PRODUCT_ID'],"VISIBLE"=>$_POST['OP_ID']])==1)
                           {
                             $account_ref = $this->where->select_where("accounts",["REFRENCE"=>$_POST['REF']],"AMOUNT");
                             if((int)$account_ref[0]['AMOUNT'] == (int)($_POST['QUANTITY']*$_POST['RATE']))
@@ -179,7 +194,9 @@ class Invoice extends CI_Controller
                                 $response = array("status"=>1,"response"=>array("ENTRY_ID"=>$id),"errors"=>array("count"=>0,null));
                                 echo json_encode($response);
                                 $final_amount = $this->where->select_where("accounts","REFRENCE IN (SELECT REF FROM invoice WHERE INVOICE_NO =".$_POST['INVOICE_NO'].") AND REFRENCE !=''","SUM(AMOUNT) AS AMOUNT");
-                                $this->where->update_where("invoice_summary",["INVOICE_NO"=>$_POST['INVOICE_NO']],["AMOUNT" => $final_amount[0]['AMOUNT']]);
+                                $final_tax = $this->where->select_where("accounts","REFRENCE LIKE '%tax_".$_POST['INVOICE_NO']."' AND REFRENCE !=''","SUM(AMOUNT) AS AMOUNT");
+                                $final_discount = $this->where->select_where("accounts","REFRENCE IN (SELECT DISCOUNT_REF FROM invoice WHERE INVOICE_NO =".$_POST['INVOICE_NO'].") AND REFRENCE !=''","SUM(AMOUNT) AS AMOUNT");
+                                $this->where->update_where("invoice_summary",["INVOICE_NO"=>$_POST['INVOICE_NO']],["AMOUNT" => $final_amount[0]['AMOUNT'],"TAX"=>$final_tax[0]['AMOUNT'], "DISCOUNT"=>$final_discount[0]['AMOUNT']]);
                                 }
                                 else
                                 {
@@ -201,6 +218,66 @@ class Invoice extends CI_Controller
                     }
                     else
                     {
+                        echo $this->api->errors(101);
+                    }
+                }
+            else
+                {
+                    echo $this->api->errors(100);
+                }
+        }
+        public function add_tax()
+        {
+            if($this->api->authentication())
+                {
+                    if(isset($_POST['OP_ID'], $_POST['PRODUCT_ID'], $_POST['ACCOUNT_ID'], $_POST['RATE'], $_POST['RATE_TYPE']) && !in_array("",$_POST))
+                    {
+                        if($this->where->check_where_num("products",["ID" => $_POST['PRODUCT_ID']]) == 1 && $this->where->check_where_num("accouting_ledgers",["ID"=>$_POST['ACCOUNT_ID']]) == 1)
+                        {
+                            $data = array(
+                                "PRODUCT_ID" => $_POST['PRODUCT_ID'],
+                                "ACCOUNT_ID" => $_POST['ACCOUNT_ID'],
+                                "RATE" => $_POST['RATE'],
+                                "RATE_TYPE" => $_POST['RATE_TYPE'],
+                                "STATUS" => 1,
+                                "VISIBLE" => $_POST['OP_ID']
+                            );
+                            $id = $this->insert->index("tax",$data);
+                            $response = array("status"=>1,"response"=>array("ENTRY_ID"=>$id),"errors"=>array("count"=>0,null));
+                            echo json_encode($response);
+                        }
+                        else{
+                            echo $this->api->errors(303);
+                        }
+                    }
+                    else{
+                        echo $this->api->errors(101);
+                    }
+                }
+            else
+                {
+                    echo $this->api->errors(100);
+                }
+        }
+        public function remove_tax()
+        {
+            if($this->api->authentication())
+                {
+                    if(isset($_POST['OP_ID'], $_POST['TAX_ID']))
+                    {
+                        if($this->where->check_where_num("tax",["ID" => $_POST['TAX_ID'],"VISIBLE"=>$_POST['OP_ID'],"STATUS"=>1]) == 1)
+                        {
+                            if($this->where->update_where("tax",["ID" => $_POST['TAX_ID'],"VISIBLE"=>$_POST['OP_ID']],["STATUS"=>0,"CLOSING_DATE"=>date('Y-m-d H-i-s')]))
+                            {
+                            $response = array("status"=>1,"errors"=>array("count"=>0,null));
+                            echo json_encode($response);
+                            }
+                        }
+                        else{
+                            echo $this->api->errors(303);
+                        }
+                    }
+                    else{
                         echo $this->api->errors(101);
                     }
                 }
